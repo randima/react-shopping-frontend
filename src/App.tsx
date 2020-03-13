003d714a64f86cc030fc5527bd2d716e90ec5ef3
randima sandamal <randimas@zone24x7.com>
2020-03-13 09:32:27 +0530

    Added pagination to products

--- src/App.tsx
+++ src/App.tsx
@@ -7,37 +7,17 @@ import { ProductsList } from './components/ProductsList';
 import { ProductService } from './services/products.service';
 const { Header, Footer, Content } = Layout;
 
-const productService = new ProductService();
+
 
 class App extends Component {
-  state = {
-    items: [],
-    itemCount: 1,
-    totalItems: 1,
-    pageCount: 1,
-    next: "",
-    previous: ""
-  };
-
-  componentDidMount() {
-    this.fetchTasks();
-  }
-
-  async fetchTasks() {
-    const result = await productService.getProducts(0);
-
-    if (result) {
-      this.setState(result.data);
-    }
-  }
-
+  
 
   render() {
     return (
       <div className="App">
         <Layout>
           <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}><TopBar></TopBar></Header>
-          <Content style={{ padding: '50px 50px', marginTop: 64 }}><ProductsList data={this.state}></ProductsList></Content>
+          <Content style={{ padding: '50px 50px', marginTop: 64 }}><ProductsList></ProductsList></Content>
           <Footer>Footer</Footer>
         </Layout>        
       </div>
--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,28 +1,69 @@
-import { FunctionComponent } from "react";
+import { Component } from "react";
 import React from "react";
-import { List, Card } from 'antd';
+import { List } from 'antd';
 import { ProductItem } from "./ProductItem";
-import { Product } from "../models/product.model";
 import { PaginationProduct } from "../models/pagination.product.model";
+import { Pagination } from 'antd';
+import { ProductService } from "../services/products.service";
+
 
 interface Props {
     data: PaginationProduct
+    loadPage: (page: number) => void
 }
 
-export const ProductsList: FunctionComponent<Props> = ({ data }) => {
-    return (
-        <div>
+const productService = new ProductService();
 
-            <List
-                grid={{ gutter: 16, column: 4 }}
-                dataSource={data.items}
-                renderItem={item => (
-                    <List.Item>
-                        <ProductItem item={item}></ProductItem>
-                    </List.Item>
-                )}
-            />
-        </div>
-    );
+export class ProductsList extends Component {
+    state = {
+        data: {
+            items: [],
+            itemCount: 1,
+            totalItems: 1,
+            pageCount: 1,
+            next: "",
+            previous: ""
+        },
+        current: 1
+    };
 
+    componentDidMount() {
+        this.fetchTasks(1);
+    }
+
+    private async fetchTasks(page: number) {
+        const result = await productService.getProducts(page);
+
+        if (result) {
+            this.setState({ data: result.data });
+        }
+    }
+
+
+    private onChange = (page: number) => {
+        console.log(page);
+        this.fetchTasks(page);
+        this.setState({
+            current: page,
+        });
+    };
+
+    render() {
+        return (
+
+            <div>
+
+                <List
+                    grid={{ gutter: 16, column: 4 }}
+                    dataSource={this.state.data.items}
+                    renderItem={item => (
+                        <List.Item>
+                            <ProductItem item={item}></ProductItem>
+                        </List.Item>
+                    )}
+                />
+                <Pagination current={this.state.current} onChange={this.onChange} total={this.state.data.totalItems} pageSize={8} />;
+            </div>
+        );
+    }
 };
